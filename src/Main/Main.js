b14458d5f24519f2ef867c40419f80db41f72894
Rodrigo Maafs <rodrigo@ticketfactura.com>
2021-04-25 00:31:15 -0500

    Se finalizó convertir mi HTML a React

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -6,6 +6,15 @@ const Main = () => {
     <React.Fragment>
       <div class="d-md-flex border-md-bottom">
         <LeftSidebar />
+
+        <div className="col-md-7 col-lg-8 col-xl-9 px-4 py-6 px-lg-7 border-top border-md-top-0 bg-gray-light">
+          <div className="mx-auto" style={{ maxWidth: 900 }}>
+            <h2>Sitio en desarrollo</h2>
+            <p class="f4 mb-4 text-gray">
+              Estoy creando esta página en mis tiempos libres :)
+            </p>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
