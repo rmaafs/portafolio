e4e560b59f2926dcd549291939f68324810d5629
Rodrigo Maafs <rodrigo@ticketfactura.com>
2021-05-01 02:03:09 -0500

    Animación al cargar la página

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,7 +2,19 @@ import React, { useState, useEffect } from "react";
 import AboutMe from "./AboutMe/AboutMe";
 import Presentacion from "./Presentacion/Presentacion";
 import Repositorios from "./Repositorios/Repositorios";
-import { PointSpreadLoading } from "react-loadingg";
+import {
+  PointSpreadLoading,
+  CommonLoading,
+  WindMillLoading,
+  MeteorRainLoading,
+  SemipolarLoading,
+  SolarSystemLoading,
+  BatteryLoading,
+  CoffeeLoading,
+  EatLoading,
+} from "react-loadingg";
+import FadeIn from "react-fade-in";
+import Colors from "../Colors";
 
 const Main = () => {
   const [loaded, setLoaded] = useState(false); //¿La página ha sido cargada?
@@ -10,20 +22,47 @@ const Main = () => {
   useEffect(() => {
     setTimeout(() => {
       setLoaded(true);
-    }, 1000);
+    }, 3000);
   });
 
-  if (loaded) {
-    return (
-      <div className="container-xs no-padding">
-        <Presentacion />
-        <AboutMe />
-        <Repositorios />
-      </div>
-    );
-  } else {
-    return <PointSpreadLoading />;
+  //Lista de loaders que obtendremos para la animación que está cargando la página
+  const listLoaders = [
+    <PointSpreadLoading color={Colors.colors.primary} />,
+    <CommonLoading color={Colors.colors.primary} />,
+    <WindMillLoading color={Colors.colors.primary} />,
+    <MeteorRainLoading color={Colors.colors.primary} />,
+    <SemipolarLoading color={Colors.colors.primary} />,
+    <SolarSystemLoading color={Colors.colors.primary} />,
+    <BatteryLoading color={Colors.colors.primary} />,
+    <CoffeeLoading color={Colors.colors.primary} />,
+    <EatLoading color={Colors.colors.primary} />,
+  ];
+
+  function getRandLoader() {
+    //Obtenemos número random en un rango de 2 números
+    function getRand(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    //Retornamos algún loader aleatorio de la lista
+    return listLoaders[getRand(0, listLoaders.length - 1)];
   }
+
+  return (
+    <React.Fragment>
+      {loaded ? null : getRandLoader()}
+      <FadeIn transitionDuration={1000} visible={loaded}>
+        <div
+          className="container-xs no-padding"
+          style={{ display: loaded ? "block" : "none" }}
+        >
+          <Presentacion />
+          <AboutMe />
+          <Repositorios />
+        </div>
+      </FadeIn>
+    </React.Fragment>
+  );
 };
 
 export default Main;
