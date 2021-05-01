8bbd88c6e5590a01f05bdb5a49fc06fad049641d
Rodrigo Maafs <rodrigo@ticketfactura.com>
2021-05-01 00:34:33 -0500

    Animación al cargar la página

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,16 +1,29 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import AboutMe from "./AboutMe/AboutMe";
 import Presentacion from "./Presentacion/Presentacion";
 import Repositorios from "./Repositorios/Repositorios";
+import { PointSpreadLoading } from "react-loadingg";
 
 const Main = () => {
-  return (
-    <div className="container-xs no-padding">
-      <Presentacion />
-      <AboutMe />
-      <Repositorios />
-    </div>
-  );
+  const [loaded, setLoaded] = useState(false); //¿La página ha sido cargada?
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 1000);
+  });
+
+  if (loaded) {
+    return (
+      <div className="container-xs no-padding">
+        <Presentacion />
+        <AboutMe />
+        <Repositorios />
+      </div>
+    );
+  } else {
+    return <PointSpreadLoading />;
+  }
 };
 
 export default Main;
