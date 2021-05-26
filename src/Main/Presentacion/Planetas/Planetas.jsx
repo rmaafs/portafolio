ec3de948753d1a4234d7fe8bae32148b70d03ada
Rodrigo Maafs <rodrigo@ticketfactura.com>
2021-05-26 00:38:29 -0500

    Tootip a los planetas

--- src/Main/Presentacion/Planetas/Planetas.jsx
+++ src/Main/Presentacion/Planetas/Planetas.jsx
@@ -1,20 +1,29 @@
+import Tooltip from "@material-ui/core/Tooltip";
 import React from 'react';
 import "./Planetas.css";
 
 const Planetas = () => {
     return ( 
         <div id="orbits">
+            <Tooltip title={"Tierra"} arrow>
             <div className="hearth">
-            <div className="luna"></div>
+                <div className="luna"></div>
             </div>
-            <div className="mars"></div>
-            <div className="jupiter">
-              <div className="luna1"></div>
-              <div className="luna2"></div>
-              <div className="luna3"></div>
-              <div className="luna4"></div>
-            </div>
-          </div>
+            </Tooltip>
+            
+            <Tooltip title={"Marte"} arrow>
+                <div className="mars"></div>
+            </Tooltip>
+
+            <Tooltip title={"Jupiter"} arrow>
+                <div className="jupiter">
+                <div className="luna1"></div>
+                <div className="luna2"></div>
+                <div className="luna3"></div>
+                <div className="luna4"></div>
+                </div>
+            </Tooltip>
+        </div>
      );
 }
  
