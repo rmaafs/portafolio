ba6bc6af7697fa1a6f200525c622fcdf63eb9a40
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-06-19 23:59:16 -0500

    Auto-formatear con prettier

--- src/Main/Presentacion/Planetas/Planetas.jsx
+++ src/Main/Presentacion/Planetas/Planetas.jsx
@@ -1,30 +1,30 @@
 import Tooltip from "@material-ui/core/Tooltip";
-import React from 'react';
+import React from "react";
 import "./Planetas.css";
 
 const Planetas = () => {
-    return ( 
-        <div id="orbits">
-            <Tooltip title={"Tierra"} arrow>
-            <div className="hearth">
-                <div className="luna"></div>
-            </div>
-            </Tooltip>
-            
-            <Tooltip title={"Marte"} arrow>
-                <div className="mars"></div>
-            </Tooltip>
-
-            <Tooltip title={"Jupiter"} arrow>
-                <div className="jupiter">
-                <div className="luna1"></div>
-                <div className="luna2"></div>
-                <div className="luna3"></div>
-                <div className="luna4"></div>
-                </div>
-            </Tooltip>
+  return (
+    <div id="orbits">
+      <Tooltip title={"Tierra"} arrow>
+        <div className="hearth">
+          <div className="luna"></div>
         </div>
-     );
-}
- 
+      </Tooltip>
+
+      <Tooltip title={"Marte"} arrow>
+        <div className="mars"></div>
+      </Tooltip>
+
+      <Tooltip title={"Jupiter"} arrow>
+        <div className="jupiter">
+          <div className="luna1"></div>
+          <div className="luna2"></div>
+          <div className="luna3"></div>
+          <div className="luna4"></div>
+        </div>
+      </Tooltip>
+    </div>
+  );
+};
+
 export default Planetas;
