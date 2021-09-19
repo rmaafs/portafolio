2454b208c1229ece6806b297045d7b059640a0bc
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-09-19 02:48:53 -0500

    Timer bar

--- src/Main/Spotify/Card/Card.jsx
+++ src/Main/Spotify/Card/Card.jsx
@@ -15,6 +15,21 @@ const Card = ({ track }) => {
           <h2>{track.name}</h2>
           <h3>{track.artista}</h3>
         </div>
+
+        <div className="sp-progress-container col-md-12 row">
+          <div className="col-md-2 sp-progress-time">0:00</div>
+          <div className="col-md-8">
+            <div className="sp-progress text-left">
+              <div
+                className="sp-progress-bar"
+                style={{
+                  width: "40%",
+                }}
+              ></div>
+            </div>
+          </div>
+          <div className="col-md-2 sp-progress-time">4:03</div>
+        </div>
       </Fragment>
     );
   };
