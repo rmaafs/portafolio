649298faa4aed91029fdc559a10fdce732654eca
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-09-19 02:26:53 -0500

    Spin bars

--- src/Main/Spotify/Card/Card.jsx
+++ src/Main/Spotify/Card/Card.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
+import SpinBars from "../SpinBars/SpinBars";
 import "./Card.css";
 
 const Card = ({ track }) => {
+  const RenderInfo = () => {
+    return (
+      <Fragment>
+        <SpinBars />
+
+        <div className="sp-img-container">
+          <img src={track.image.url} />
+        </div>
+        <div className="sp-track text-left">
+          <h2>{track.name}</h2>
+          <h3>{track.artista}</h3>
+        </div>
+      </Fragment>
+    );
+  };
+
   return (
-    <div className="sp-card row">
-      <div className="sp-img-container">
-        <img src={track.image.url} />
-      </div>
-      <div className="sp-track text-left">
-        <h2>{track.name}</h2>
-        <h3>{track.artista}</h3>
-      </div>
-    </div>
+    <div className="sp-card row">{track && track.image && <RenderInfo />}</div>
   );
 };
 
