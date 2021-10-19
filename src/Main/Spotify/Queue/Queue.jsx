893141a4c894aea99267a83fcff93a582d2f239c
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-10-18 22:34:27 -0500

    Botón para recomendar

---
+++ src/Main/Spotify/Queue/Queue.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Queue.css";
+
+const Queue = () => {
+  return (
+    <div className="queue">
+      <div className="recomendar">
+        <i className={"fa fa-plus fa-fw mr-2 v-align-middle"}></i>
+        Recomendar canción
+      </div>
+    </div>
+  );
+};
+
+export default Queue;
