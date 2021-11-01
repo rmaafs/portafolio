3e68798831e2a124dee0e91d41848cc276627ae9
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-11-01 00:10:33 -0600

    Precargar gorrito en navidad

--- src/Main/PreImageLoader/PreImageLoader.jsx
+++ src/Main/PreImageLoader/PreImageLoader.jsx
@@ -5,13 +5,19 @@ import imgPaisaje from "../../assets/yo_paisaje.png";
 import gorrito from "../../assets/gorro.png";
 
 const PreImageLoader = ({ onFinish }) => {
+  const isNavidad = new Date().getMonth() + 1 === 12;
+
   const imagesPreload = [
     imgEuropa,
     "https://avatars.githubusercontent.com/u/47652130?s=300&u=e14efd0723cc525183525917bb108188e2cc1bde&v=4",
     imgPaisaje,
-    gorrito,
   ];
   let imagesLoaded = 0;
+
+  //Si es Navidad, precargamos el gorrito
+  if (isNavidad) {
+    imagesPreload.push(gorrito);
+  }
 
   useEffect(() => {
     setTimeout(() => {
