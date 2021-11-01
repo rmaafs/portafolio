1603aef2060b6a64790971d60beaa1c998a2a015
Rodrigo Maafs Atilano <rodrigoelmaps@gmail.com>
2021-10-31 23:45:52 -0600

    Componente para precargar fotos pesadas

---
+++ src/Main/PreImageLoader/PreImageLoader.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+
+import imgEuropa from "../../assets/europa.png";
+import imgPaisaje from "../../assets/yo_paisaje.png";
+
+const PreImageLoader = ({ onFinish }) => {
+  const imagesPreload = [
+    imgEuropa,
+    "https://avatars.githubusercontent.com/u/47652130?s=300&u=e14efd0723cc525183525917bb108188e2cc1bde&v=4",
+    imgPaisaje,
+  ];
+  let imagesLoaded = 0;
+
+  useEffect(() => {
+    setTimeout(() => {
+      preLoadImages();
+    }, 500);
+  }, []);
+
+  const readyImg = () => {
+    if (++imagesLoaded === imagesPreload.length) {
+      onFinish();
+    }
+  };
+
+  function preLoadImages() {
+    imagesPreload.forEach((image) => {
+      const newImage = new Image();
+      newImage.src = image;
+      newImage.onload = readyImg();
+      window[image] = newImage;
+    });
+  }
+
+  return <div></div>;
+};
+
+export default PreImageLoader;
